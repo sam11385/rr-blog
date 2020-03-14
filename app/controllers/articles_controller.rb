0842fb4537c26bb0b78491ba85685db88e29d60d
sam11385 <samluedke@gmail.com>
2020-03-14 08:38:18 -0600

    Create index for articles, route to it via some standard dynamic linking

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,14 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+
     def show
         @article = Article.find(params[:id])
     end
 
     def new
     end
-
     
     def create
         @article = Article.new(article_params)
